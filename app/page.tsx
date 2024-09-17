ec32cdbc2040cae2f17791f681ad34cfd4e65931
Thanakorn Chareonlertkamol <t.chareonlertkamol@gmail.com>
2024-09-17 20:28:37 +0700

    update text index

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>DevjinX</h1>
+    <main className="flex items-center justify-center min-h-screen text-white">
+        <header className="text-center">
+          <h1 className="text-4xl font-bold mb-2 text-justify">
+            Thanakorn Chareonlertkamol
+          </h1>
+          <h2 className="text-2xl font-medium mb-2 text-justify">
+            Full Stack Developer
+          </h2>
+          <p className="text-lg max-w-lg mx-auto text-justify">
+            I create a better experience for everyone in using technology.
+          </p>
+        </header>
     </main>
   );
 }
