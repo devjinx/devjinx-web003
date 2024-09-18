b7cfe73ba7af9457730347e81214a7c89bd47f6e
Thanakorn Chareonlertkamol <t.chareonlertkamol@gmail.com>
2024-09-18 16:40:16 +0700

    update postion of all class

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen text-white">
-        <header className="text-center">
-          <h1 className="text-4xl font-bold mb-2 text-justify">
-            Thanakorn Chareonlertkamol
-          </h1>
-          <h2 className="text-2xl font-medium mb-2 text-justify">
-            Full Stack Developer
-          </h2>
-          <p className="text-lg max-w-lg mx-auto text-justify">
-            I create a better experience for everyone in using technology.
+    <div className="flex justify-center mt-[100px] min-h-screen">
+      <div id="left" className="flex-1 max-w-[500px] px-4" style={{ marginBottom: '300px' }}>
+        <main>
+          <header>
+            <h1 className="text-3xl font-bold mb-2 text-white">
+              Thanakorn Chareonlertkamol
+            </h1>
+            <h2 className="text-xl mb-2 text-white">
+              Full Stack Developer
+            </h2>
+            <p className="text-base mb-2 text-white">
+              I create a better experience for everyone in using technology.
+            </p>
+          </header>
+        </main>
+      </div>
+      <div id="right" className="flex-1 max-w-[500px] px-4 ml-8">
+        <div>
+          <p className="text-white">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
-        </header>
-    </main>
+        </div>
+      </div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      margin: {
+        '100': '100px', // Add custom margin value here
+      },
     },
   },
   plugins: [],
