bc8fc6873e2cf816004b383441425c067d4c1367
Thanakorn Chareonlertkamol <t.chareonlertkamol@gmail.com>
2024-09-25 17:49:47 +0700

    update all file andnavbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,28 +1,38 @@
-import Link from "next/link";
-import './navbar.css'
+import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
-    return(
-      <nav>
-        <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/archive">
-                  <p>Archive</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/blog">
-                  <p>Blog</p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      </nav>
-    );
-}
+  return (
+    <nav className="bg-opacity-90 w-48 h-full z-50 flex flex-col items-start px-5 py-5">
+      <ul className="flex flex-col space-y-5 text-white font-medium">
+        <li>
+          <Link href="#about" aria-label="About section" className="hover:text-[#5dfdcb] transition duration-300">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="#experience" aria-label="Experience section" className="hover:text-[#5dfdcb] transition duration-300">
+            Experience
+          </Link>
+        </li>
+        <li>
+          <Link href="#project" aria-label="Project section" className="hover:text-[#5dfdcb] transition duration-300">
+            Project
+          </Link>
+        </li>
+        <li>
+          <Link href="/archivement" aria-label="Achievements page" className="hover:text-[#5dfdcb] transition duration-300">
+            Achievement
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog" aria-label="Blog page" className="hover:text-[#5dfdcb] transition duration-300">
+            Blog
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,47 @@ import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 
-
 export default function index() {
   return (
     <>
-    <Navbar />
-    <div className="flex justify-center mt-[100px] min-h-screen">
-      <div id="left" className="flex-1 max-w-[500px] px-4" style={{ marginBottom: ' 500px' }}>
-        <main>
-          <header>
-            <h1 className="text-3xl font-bold mb-2 text-white">
-              Thanakorn Chareonlertkamol
-            </h1>
-            <h2 className="text-xl mb-2 text-white">
-              Full Stack Developer
-            </h2>
-            <p className="text-base mb-2 text-white">
-              I create a better experience for everyone in using technology.
+      <div className="flex justify-center mt-[100px] min-h-screen">
+        <div id="left" className="flex-1 max-w-[600px] px-4" style={{ marginBottom: '600px' }}>
+          <main>
+            <header>
+              <h1 className="text-4xl font-bold mb-2 text-white">
+                Thanakorn Chareonlertkamol
+              </h1>
+              <h2 className="text-xl mb-2 text-white">
+                Full Stack Developer
+              </h2>
+              <p className="text-base mb-2 text-white">
+                I create a better experience for everyone in using technology.
+              </p>
+            </header>
+          </main>
+          <Navbar />
+        </div>
+
+        <div id="right" className="flex-1 max-w-[600px] px-4 ml-8">
+          <div id='about'>
+            <p className="text-white text-base pb-2">
+              My name is Thanakorn Chareonlertkamol, though most people know me as Jin. I am currently pursuing my degree at King Mongkut's University of Technology Thonburi (KMUTT), specializing in Information Technology. Throughout my academic journey, I have developed a deep passion for both coding and cooking, appreciating the challenge, precision, and creativity that both disciplines demand.
             </p>
-          </header>
-        </main>
-      </div>
-      <div id="right" className="flex-1 max-w-[500px] px-4 ml-8">
-        <div>
-          <p className="text-white">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+            <p className="text-white text-base pb-2">
+              As a full-stack developer, I have gained extensive hands-on experience in a variety of modern technologies, including Next.js, React.js, Vue.js, TypeScript, Express.js, SQL. Additionally, I am proficient in leveraging cloud services such as Firebase and Cloudflare, as well as databases like MongoDB and MySQL, to create efficient, scalable, and secure web applications. My design toolkit includes frameworks like Tailwind CSS and Bootstrap, allowing me to develop user-friendly interfaces with clean, responsive designs.
+            </p>
+            <p className="text-white text-base pb-10">
+              I am deeply committed to continuous learning and professional growth. My ultimate goal is to advance into a senior management position within the IT industry and eventually establish my own IT company. I believe that attention to detail is crucial to success, and I apply this principle in every project I undertake. Guided by the motto, "Hard work produces luck and success," I am dedicated to achieving my career aspirations through perseverance, innovation, and a constant drive for improvement.
+            </p>
+          </div>
+          <div id='experience'> 
+          </div>
+          <div id='project'>
+          </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      
+      <Footer />
     </>
   );
 }
